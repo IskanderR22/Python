9f667181c24a433ddee37d314cd5d8ecdda95309
Iskander Rangel <icerangel@outlook.com>
2021-07-06 13:29:18 -0700

    W3 D2

--- Python_Assignments/For_loopbasics.py
+++ Python_Assignments/For_loopbasics.py
@@ -15,22 +15,22 @@ for i in range(1, 101):
         print("Coding")
         
 
-sumOfOdds = 0
+sum_of_odds = 0
     
 for i in range(0, 500001):
     if i % 2 != 0:
-        sumOfOdds += i
+        sum_of_odds += i
 
-print(sumOfOdds)
+print(sum_of_odds)
 
 
-for i in range(2018, 0, -4): 
-    print(i)    
+# for i in range(2018, 0, -4): 
+#     print(i)    
 
-low_num = 2
-high_num = 10
-mult = 2
+# low_num = 2
+# high_num = 10
+# mult = 2
 
-for i in range(low_num, high_num):          
-    if i % mult == 0:
-        print(i)
+# for i in range(low_num, high_num):          
+#     if i % mult == 0:
+#         print(i)
