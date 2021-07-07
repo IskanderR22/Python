da3adf75018a731e639cd4c3f6f00526543ecf47
Iskander Rangel <icerangel@outlook.com>
2021-07-07 12:06:58 -0700

    W3 D2

--- Python_Assignments/method_chaining.py
+++ Python_Assignments/method_chaining.py
@@ -9,31 +9,27 @@ class User:
     
     def make_deposit(self, deposit):
         self.balance = self.balance + deposit
+        return self
     
     def make_withdrawl(self, withdrawl):
         self.balance = self.balance - withdrawl
+        return self
 
     def display_balance(self):
         print(self.balance)
 
 
 micheal = User("Micheal", "micheal@gmail.com", 1000)
-micheal.make_deposit(1000)
-micheal.make_deposit(1000)
-micheal.make_deposit(1000)
+micheal.make_deposit(1000).make_deposit(1000).make_deposit(1000)
 micheal.make_withdrawl(500)
 micheal.display_balance()
 
 harry = User("Harry", "harry@gmail.com", 5000)
-harry.make_deposit(100)
-harry.make_deposit(100)
-harry.make_withdrawl(1000)
-harry.make_withdrawl(1000)
+harry.make_deposit(100).make_deposit(100)
+harry.make_withdrawl(1000).make_withdrawl(1000)
 harry.display_balance()
 
 josh = User("Josh", "josh@gmail.com", 3000)
 josh.make_deposit(10000)
-josh.make_withdrawl(1000)
-josh.make_withdrawl(1000)
-josh.make_withdrawl(1000)
+josh.make_withdrawl(1000).make_withdrawl(1000).make_withdrawl(1000)
 josh.display_balance()
