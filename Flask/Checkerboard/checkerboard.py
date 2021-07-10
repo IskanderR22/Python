0a6f2ca4dd6d8cc0688c18ae0873b4f40ee064c1
Iskander Rangel <icerangel@outlook.com>
2021-07-09 17:06:47 -0700

    W3 D5

--- Flask/Checkerboard/checkerboard.py
+++ Flask/Checkerboard/checkerboard.py
@@ -4,9 +4,9 @@ app = Flask(__name__)    # Create a new instance of the Flask class called "app"
 
 @app.route('/')
 @app.route('/<int:y>')
-@app.route('/<int:x>/<int:y>') # i googled this 
+@app.route('/<int:x>/<int:y>') 
 @app.route('/<int:x>/<int:y>/<string:color1>/<string:color2>')
-def checkerboard(x=8, y=8, color1 = "blue", color2 = "yellow"):
+def checkerboard(x=8, y=8, color1 = "red", color2 = "black"):
     
     return render_template("index.html", x=x,y=y, color1 = color1, color2 = color2)
 
